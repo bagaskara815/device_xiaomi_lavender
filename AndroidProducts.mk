6b8fb8044f600e0346277518e4ee5e28d628ce68
bagaskara815 <bagaskara815@gmail.com>
2021-02-24 03:43:46 +0000

    Nos-ify
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -23,9 +23,9 @@
 
 # Inherit Redmi Note 7 device config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_lavender.mk
+    $(LOCAL_DIR)/nitrogen_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_lavender-eng \
-    evolution_lavender-userdebug \
-    evolution_lavender-user
+    nitrogen_lavender-eng \
+    nitrogen_lavender-userdebug \
+    nitrogen_lavender-user
